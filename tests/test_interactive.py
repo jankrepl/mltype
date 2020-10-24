3f0f6987861cc7b3230bc906abb3a50654818000
Jan Krepl <jankrepl@yahoo.com>
2020-10-24 19:28:28 +0200

    Initial version

--- tests/test_interactive.py
+++ tests/test_interactive.py
@@ -1,8 +1,69 @@
 """Collection of tests covering the `interactive.py` module."""
 from unittest.mock import Mock, MagicMock
 
+import pytest
+
+try:
+    import hecate.hecate
+
+    RUN_HECATE = True
+
+except ImportError:
+    RUN_HECATE = False
+
 from mltype.base import TypedText
 from mltype.interactive import main_basic
+
+font_colors = {
+    "black": 30,
+    "red": 31,
+    "green": 32,
+    "yellow": 33,
+    "blue": 34,
+    "magenta": 35,
+    "cyan": 36,
+    "white": 37,
+    "reset": 0,
+}
+
+
+background_colors = {
+    "black": 40,
+    "red": 41,
+    "green": 42,
+    "yellow": 43,
+    "blue": 44,
+    "magenta": 45,
+    "cyan": 46,
+    "white": 47,
+}
+
+DEFAULT_BACKGROUND = "black"
+
+
+def esc(x):
+    return f"\033[{x}m"
+
+
+def capture_pane_color(self, pane):
+    """Screenshot the entire tmux pane with colors.
+
+    We patch the original `hackate` method with this one
+    in order to be capture colors too.
+
+    Parameters
+    ----------
+    pane : int
+        Number of the pane.
+
+    Return
+    ------
+    str
+        The screnshot with colors.
+    """
+    buf = self.a_buffer()
+    self.execute_command("capture-pane", "-e", "-b", buf, "-t", pane)
+    return self.get_buffer(buf)
 
 
 def test_strip(monkeypatch):
@@ -15,3 +76,49 @@ def test_strip(monkeypatch):
 
     main_basic(fake_text, None, None, None, None)
     fake_text.rstrip.assert_called_once()
+
+
+class TestHecate:
+    @pytest.mark.skipif(not RUN_HECATE, reason="Hecate is not installed")
+    def test_basic(self, monkeypatch):
+        monkeypatch.setattr(
+            hecate.hecate.Tmux, "capture_pane", capture_pane_color
+        )
+
+        with hecate.hecate.Runner("mlt", "raw", "inside") as r:
+            # initial check
+            s_initial = esc(font_colors["white"])
+            s_initial += esc(background_colors[DEFAULT_BACKGROUND])
+            s_initial += "inside"
+            r.await_text(s_initial, 1)
+
+            # type i
+            r.write("i")
+            s = esc(font_colors["green"])
+            s += esc(background_colors[DEFAULT_BACKGROUND])
+            s += "i"
+            s += esc(font_colors["white"])
+            s = "nside"
+            r.await_text(s, 1)
+
+            # backspace
+            r.press("BSpace")
+            r.await_text(s_initial, 1)
+
+            # type insa
+            r.write("insa")
+            s = esc(font_colors["green"])
+            s += esc(background_colors[DEFAULT_BACKGROUND])
+            s += "ins"
+            s = esc(font_colors["white"])
+            s += esc(background_colors["red"])
+            s += "i"
+            s += esc(background_colors[DEFAULT_BACKGROUND])
+            s += "de"
+            r.await_text(s, 1)
+
+            # Finalize
+            r.press("BSpace")
+            r.write("ide")
+
+            r.await_text("== Statistics ==")
